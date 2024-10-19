4753162f9568986823dfed719a8397950d0b1604
andrewshillito <andrew.shillito.dev@gmail.com>
2024-10-19 15:00:52 -0400

    Define hierarchical user groups authority structure

--- src/main/java/com/demo/security/spring/utils/AuthorityGroups.java
+++ src/main/java/com/demo/security/spring/utils/AuthorityGroups.java
@@ -3,8 +3,19 @@ package com.demo.security.spring.utils;
 import static com.demo.security.spring.utils.AuthorityAdminRoles.*;
 import static com.demo.security.spring.utils.AuthorityUserRoles.*;
 
+import com.google.common.collect.ImmutableSet;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+/**
+ * The sets in this class result in a hierarchical roles structure
+ * GROUP_ADMIN_SYSTEM < GROUP_ADMIN_USERS < GROUP_ACCOUNT_HOLDER < GROUP_USER
+ * GROUP_ADMIN_SYSTEM < GROUP_ADMIN_ACCOUNTS < GROUP_ACCOUNT_HOLDER < GROUP_USER
+ * GROUP_ADMIN_SYSTEM < GROUP_ADMIN_CARDS < GROUP_ACCOUNT_HOLDER < GROUP_USER
+ * GROUP_ADMIN_SYSTEM < GROUP_ADMIN_LOANS < GROUP_ACCOUNT_HOLDER < GROUP_USER
+ * GROUP_ADMIN_SYSTEM < GROUP_ADMIN_TRANSACTIONS < GROUP_ACCOUNT_HOLDER < GROUP_USER
+ */
 public class AuthorityGroups {
 
   /** A group of users who have basic privileges to view and edit their own user information */
@@ -22,8 +33,7 @@ public class AuthorityGroups {
   public static final String GROUP_ACCOUNT_HOLDER = "GROUP_ACCOUNT_HOLDER";
 
   /** The set of rules which is granted if you belong to the ADMIN_USER group */
-  public static final Set<String> GROUP_ACCOUNT_HOLDER_ROLES = Set.of(
-      ROLE_USER,
+  public static final Set<String> GROUP_ACCOUNT_HOLDER_ROLES = addAll(Set.of(
       ROLE_SELF_LOAN_VIEW,
       ROLE_SELF_LOAN_APPLY,
       ROLE_SELF_LOAN_EDIT,
@@ -36,80 +46,93 @@ public class AuthorityGroups {
       ROLE_SELF_TRANSACTION_VIEW,
       ROLE_SELF_TRANSACTION_CREATE,
       ROLE_SELF_TRANSACTION_EDIT
-  );
+  ), GROUP_USER_ROLES);
 
   /** A group of user roles which allows administrating user accounts */
   public static final String GROUP_ADMIN_USERS = "GROUP_ADMIN_USERS";
 
   /** The set of rules which is granted if you belong to the ADMIN_USERS group */
-  public static final Set<String> GROUP_ADMIN_USERS_ROLES = Set.of(
-      ROLE_USER,
+  public static final Set<String> GROUP_ADMIN_USERS_ROLES = addAll(Set.of(
       ROLE_ADMIN,
       ROLE_ADMIN_USERS_VIEW,
       ROLE_ADMIN_USERS_EDIT
-  );
+  ), GROUP_ACCOUNT_HOLDER_ROLES);
 
   /** A group of user roles which allows administrating user accounts */
   public static final String GROUP_ADMIN_ACCOUNTS = "GROUP_ADMIN_ACCOUNTS";
 
   /** The set of rules which is granted if you belong to the ADMIN_ACCOUNTS group */
-  public static final Set<String> GROUP_ADMIN_ACCOUNTS_ROLES = Set.of(
-      ROLE_USER,
+  public static final Set<String> GROUP_ADMIN_ACCOUNTS_ROLES = addAll(Set.of(
       ROLE_ADMIN,
       ROLE_ADMIN_ACCOUNTS_VIEW,
       ROLE_ADMIN_ACCOUNTS_EDIT
-  );
+  ), GROUP_ACCOUNT_HOLDER_ROLES);
 
   /** A group of user roles which allows administrating user cards */
   public static final String GROUP_ADMIN_CARDS = "GROUP_ADMIN_CARDS";
 
   /** The set of rules which is granted if you belong to the ADMIN_CARDS group */
-  public static final Set<String> GROUP_ADMIN_CARDS_ROLES = Set.of(
-      ROLE_USER,
+  public static final Set<String> GROUP_ADMIN_CARDS_ROLES = addAll(Set.of(
       ROLE_ADMIN,
       ROLE_ADMIN_CARDS_VIEW,
       ROLE_ADMIN_CARDS_EDIT
-  );
+  ), GROUP_ACCOUNT_HOLDER_ROLES);
 
   /** A group of user roles which allows administrating user loans */
   public static final String GROUP_ADMIN_LOANS = "GROUP_ADMIN_LOANS";
 
   /** The set of rules which is granted if you belong to the ADMIN_LOANS group */
-  public static final Set<String> GROUP_ADMIN_LOANS_ROLES = Set.of(
-      ROLE_USER,
+  public static final Set<String> GROUP_ADMIN_LOANS_ROLES = addAll(Set.of(
       ROLE_ADMIN,
       ROLE_ADMIN_LOANS_VIEW,
       ROLE_ADMIN_LOANS_EDIT
-  );
+  ), GROUP_ACCOUNT_HOLDER_ROLES);
 
   /** A group of user roles which allows administrating user account transactions */
   public static final String GROUP_ADMIN_TRANSACTIONS = "GROUP_ADMIN_TRANSACTIONS";
 
   /** The set of rules which is granted if you belong to the ADMIN_TRANSACTIONS group */
-  public static final Set<String> GROUP_ADMIN_TRANSACTIONS_ROLES = Set.of(
-      ROLE_USER,
+  public static final Set<String> GROUP_ADMIN_TRANSACTIONS_ROLES = addAll(Set.of(
       ROLE_ADMIN,
       ROLE_ADMIN_TRANSACTIONS_VIEW,
       ROLE_ADMIN_TRANSACTIONS_EDIT
-  );
+  ), GROUP_ACCOUNT_HOLDER_ROLES);
 
   /** A group of user roles which allows administrating the entire application */
   public static final String GROUP_ADMIN_SYSTEM = "GROUP_ADMIN_SYSTEM";
 
   /** The set of rules which is granted if you belong to the ADMIN_SYSTEM group */
-  public static final Set<String> GROUP_ADMIN_SYSTEM_ROLES = Set.of(
-      ROLE_USER,
-      ROLE_ADMIN,
-      ROLE_ADMIN_USERS_VIEW,
-      ROLE_ADMIN_USERS_EDIT,
-      ROLE_ADMIN_ACCOUNTS_VIEW,
-      ROLE_ADMIN_ACCOUNTS_EDIT,
-      ROLE_ADMIN_CARDS_VIEW,
-      ROLE_ADMIN_CARDS_EDIT,
-      ROLE_ADMIN_LOANS_VIEW,
-      ROLE_ADMIN_LOANS_EDIT,
-      ROLE_ADMIN_TRANSACTIONS_VIEW,
-      ROLE_ADMIN_TRANSACTIONS_EDIT
-  );
+  public static final Set<String> GROUP_ADMIN_SYSTEM_ROLES = addAll(
+      new HashSet<>(), List.of(
+          GROUP_ADMIN_USERS_ROLES,
+          GROUP_ADMIN_ACCOUNTS_ROLES,
+          GROUP_ADMIN_CARDS_ROLES,
+          GROUP_ADMIN_LOANS_ROLES,
+          GROUP_ADMIN_TRANSACTIONS_ROLES
+      ));
+
+  static Set<String> addAll(Set<String> target, Set<String> toAdd) {
+    if (target != null) {
+      Set<String> temp = new HashSet<>(target);
+      if (toAdd != null) {
+        temp.addAll(toAdd);
+      }
+      target = ImmutableSet.copyOf(temp);
+    }
+    return target;
+  }
+
+  static Set<String> addAll(Set<String> target, List<Set<String>> toAdd) {
+    if (target != null) {
+      Set<String> temp = new HashSet<>(target);
+      if (toAdd != null && !toAdd.isEmpty()) {
+        for (Set<String> s : toAdd) {
+          temp = addAll(temp, s);
+        }
+      }
+      target = ImmutableSet.copyOf(temp);
+    }
+    return target;
+  }
 
 }
